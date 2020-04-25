6398a986ff623f18ddc12a361fff728ccdc9d66a
Scout Master <33911707+ScoutMaster4@users.noreply.github.com>
2020-04-25 20:20:33 +0200

    Update Perl3.pl

--- Perl3.pl
+++ Perl3.pl
@@ -1,12 +1,5 @@
 #!/usr/bin/perl
-#use Class::CSV;
-#$csv = Class::CSV->parse(
-#  filename => 'test.csv',
-#  fields   => [NAME/SCORE/]
-#  line_separator => "\r\n";
-#);
 package classA;
-
 open (FH, ">file2.csv") or die "$!";
 print FH "NAME, \t\tSCORE\n";
 our @n;
@@ -23,17 +16,9 @@ if(@ARGV != undef){
      {
        (@n) = $a =~ /$matchN/g;
        (@s) = $a =~ /$matchS/g;
-
-       #print "$1\n";
      }
    }
 
-#   foreach $b (@strings) {
-#     if($b =~ /td/)
-#     {
-#       (@s) = $b =~ /$matchS/g;
-#     }
-#   }
    for($i = 0; $i < scalar @n; $i++)
    {
      print FH "\"$n[$i]\", \"$s[$i]\"\n";
