33991ebe1366163729ce8fc6600ff409b1678083
Scout Master <33911707+ScoutMaster4@users.noreply.github.com>
2020-06-14 03:49:51 +0200

    Add files via upload

---
+++ PWJS_Lab6_Zad1/PWJS_Lab6_Zad1/PWJS_Lab6_Zad1.py
@@ -0,0 +1,18 @@
+# Łukasz Maćkiewicz
+# 39348
+# 31B
+# PWJS Lab6 Python
+# Zadanie na ocenę 3.0 (Zad 1 Perl namiastka 'ls')
+import os
+import sys
+
+if len(sys.argv) < 2:
+	lista = os.listdir(os.getcwd())
+	sorted_list = sorted(lista)
+	for i in sorted_list:
+		print (i)
+else:
+	lista = os.listdir(sys.argv[1])
+	sorted_list = sorted(lista)
+	for i in sorted_list:
+		print (i)
